67c320f65ee8aec73b9a69b1f187e84af059c445
Arno Schutijzer <arno.schutijzer@skryv.com>
2024-01-05 00:43:58 +0100

    get the diff

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"os"
+
+	"github.com/google/go-github/v57/github"
 )
 
 type Event struct {
+	Number      int         `json:"number"`
 	PullRequest PullRequest `json:"pull_request"`
+	Repository  Repository  `json:"repository"`
 }
 type PullRequest struct {
 	DiffURL string `json:"diff_url"`
+}
+type Repository struct {
+	Name  string `json:"name"`
+	Owner Owner  `json:"owner"`
+}
+type Owner struct {
+	Login string `json:"login"`
 }
 
 func main() {
@@ -18,8 +30,20 @@ func main() {
 	if !ok {
 		log.Fatal("could not find event, can't do anything")
 	}
+	token, ok := os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		log.Fatal("could not find token, can't do anything")
+	}
 
 	data, _ := os.ReadFile(pathToEvent)
 	var event Event
 	json.Unmarshal(data, &event)
+
+	client := github.NewClient(nil).WithAuthToken(token)
+	bg := context.Background()
+	diff, _, err := client.PullRequests.GetRaw(bg, event.Repository.Owner.Login, event.Repository.Name, event.Number, github.RawOptions{Type: github.Diff})
+	if err != nil {
+		log.Fatal("could not get diff due to: ", err)
+	}
+	log.Println(diff)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/arnoschutijzer/what-would-linus-torvalds-say
 
 go 1.21.5
+
+require github.com/google/go-github/v57 v57.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
